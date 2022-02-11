79d229f63961381e829d2918ad84e3369740a139
Notnik_KG <tasnim0800@gmail.com>
2022-02-11 12:24:27 +0600

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Set the radius of cicrle: ");
         Circle circle = new Circle(3.14, scanner.nextDouble());
         Circle.calculateArea();
         Circle.circumference();
